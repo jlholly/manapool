a1f92ed8516519facd840d875324e05f1af02d43
Josh <jlholly@users.noreply.github.com>
2025-02-08 14:40:16 -0600

    UI Storybook Autodoc Setup

--- libs/shared/ui/.storybook/main.ts
+++ libs/shared/ui/.storybook/main.ts
@@ -7,6 +7,10 @@ const config: StorybookConfig = {
     name: '@storybook/angular',
     options: {},
   },
+  docs: {
+    autodocs: true,
+    defaultName: 'Documentation',
+  }
 };
 
 export default config;
