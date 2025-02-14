7686e8630f4c1d0adc24d00c585557e8ada19170
Josh <jlholly@users.noreply.github.com>
2025-02-14 16:34:02 -0600

    Added App Shell Library

---
+++ libs/shell/src/lib/shell/shell.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-shell',
+  imports: [CommonModule],
+  templateUrl: './shell.component.html',
+  styleUrl: './shell.component.scss',
+})
+export class ShellComponent {}
